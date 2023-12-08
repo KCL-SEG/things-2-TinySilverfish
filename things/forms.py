5961a914419751b386300b0ed8f8f39bc2860b3d
TinySilverfish <tinysilverfish@gmail.com>
2023-12-08 10:07:10 +0000

    Add ThingForm to create new Thing objects

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,7 @@
-"""Forms of the project."""
+from django import forms
+from .models import Thing
 
-# Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
